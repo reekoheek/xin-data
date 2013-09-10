c05985d1f72f8bfc8fb7ecd17d86f4730d87f494
Krisan Alfa Timur <krisan47@gmail.com>
2013-09-10 14:15:09 +0700

    New Memory Adapter (stable version, but need more addition)

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-xin-data
-========
+#xin-data
 
-Xin data
+##Memory Adapter
+###Basic usage
+```javascript
+var repo = window.repo = new xin.data.Repository({}, function(err) {
+        if (err) throw new Error(err.message);
+
+        // Saving record
+        repo.save({'name': 'ani', 'age': 16}, function() {
+            console.log("Saving ani");
+        });
+
+        // Updating record
+        repo.update({"name": "ani"}, {"name": "banu"}, function() {
+            console.log("Updating ani");
+        });
+
+        // Getting record
+        repo.get({'name': 'banu'}, function(record) {
+            console.log(record);
+        });
+
+        // Getting all records
+        repo.all(function(records) {
+            console.log(records);
+        });
+
+        // Removing records
+        repo.remove({'name': 'ani'}, function(criteria) {
+            console.log(criteria.name + ' has been removed');
+        });
+
+        // Drop all records
+        repo.nuke(function() {
+            console.log(repo.all()); // Should be -> {}
+        });
+    });
+```
+###API
+(In Documentation Progress)
--- src/adapters/memory.js
+++ src/adapters/memory.js
@@ -1,31 +1,103 @@
 (function() {
 
-    var adapter = {
+    "use strict";
 
+    var storage = {};
+    var adapter = {
         name: 'memory',
 
         init: function(callback) {
-
+            var that = this;
+            setTimeout(function() {
+                if (callback) {
+                    callback(null, that);
+                }
+            });
         },
 
         save: function(object, callback) {
-
+            var id = this.uuid();
+            var index, value;
+            if (!object) {
+                var err = {
+                    code: "001",
+                    message: "Cannot save while object is null"
+                };
+                if (callback) { return callback(err, null); }
+                throw new Error(err);
+            }
+            // Creating
+            object.id = id;
+            storage[id] = object;
+            if (callback) { return callback(null, object); }
         },
 
-        get: function(key, callback) {
-
+        // FIXME: Update should updating record with much more modification
+        // Object should be wrapped in array :/
+        // So it should be: repo.update({'x':'y'}, [{'x':'z'},{'i':'j'}], callback)
+        update: function(criteria, object, callback) {
+            var index, value, updateIndex, updateValue;
+            if (!object) {
+                var err = {
+                    code: "002",
+                    message: "Will not update anything"
+                };
+                if (callback) { return callback(err, null); }
+                throw new Error(err);
+            }
+            for(var x in criteria) { index = x; value = criteria[x]; }
+            for(var y in object) { updateIndex = y; updateValue = object[y]; }
+            for(var i in storage) {
+                for(var j in storage[i]) {
+                    if (index == j && value == storage[i][j]) {
+                        storage[i][j] = updateValue;
+                        if (callback) { return callback(null, object); }
+                    }
+                }
+            }
+        },
+        get: function(options, callback) {
+            var index, value;
+            for(var x in options) {
+                index = x;
+                value = options[x];
+            }
+            for(var i in storage) {
+                for(var j in storage[i]) {
+                    if (index == j && value == storage[i][j]) {
+                        if (typeof callback == "function") { return callback(storage[i]); }
+                        return storage[i];
+                    }
+                }
+            }
+            if (typeof callback == "function") { return callback(null); }
+            return null;
         },
 
         all: function(callback) {
-
+            if (typeof callback == "function") { return callback(storage); }
+            return storage;
         },
 
-        remove: function() {
-
+        remove: function(options, callback) {
+            var index, value;
+            for(var x in options) {
+                index = x;
+                value = options[x];
+            }
+            for(var i in storage) {
+                for(var j in storage[i]) {
+                    if (index == j && value == storage[i][j]) {
+                        delete storage[i];
+                        if (typeof callback == "function") { return callback(); }
+                    }
+                }
+            }
         },
 
-        nuke: function() {
-
+        nuke: function(callback) {
+            storage = {};
+            if (typeof callback == "function") { return callback(); }
         }
     };
 
